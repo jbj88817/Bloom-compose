b872672dfeb232d3c08d78afe0b90a9bf1a1e139
Bojie Jiang <jiangbojie@gmail.com>
2022-01-01 13:57:45 -0800

    Change SystemBarsColor

--- app/src/main/java/us/bojie/bloom_compose/ui/theme/Theme.kt
+++ app/src/main/java/us/bojie/bloom_compose/ui/theme/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Green900,
@@ -49,6 +50,17 @@ fun BloomTheme(
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val systemUiController = rememberSystemUiController()
+    if (darkTheme) {
+        systemUiController.setSystemBarsColor(
+            color = Green900
+        )
+    } else {
+        systemUiController.setSystemBarsColor(
+            color = Pink100
+        )
     }
 
     MaterialTheme(
