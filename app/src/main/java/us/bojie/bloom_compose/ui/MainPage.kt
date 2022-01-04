519d19261d56d1b9bef9c3e1f30555e2cc64bd2c
Bojie Jiang <jiangbojie@gmail.com>
2022-01-03 21:16:28 -0800

    Setup mainPage

---
+++ app/src/main/java/us/bojie/bloom_compose/ui/MainPage.kt
@@ -0,0 +1,18 @@
+package us.bojie.bloom_compose.ui
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainPage() {
+    Scaffold() {
+
+    }
+}
+
+@Preview
+@Composable
+fun MainPagePreview() {
+    MainPage()
+}
