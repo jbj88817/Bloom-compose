22829dcacc51141dfd1c84a84702c98b33ee67f9
Bojie Jiang <bojie.jiang@servicenow.com>
2022-01-05 23:26:50 -0800

    Clear

--- app/src/main/java/us/bojie/bloom_compose/MainActivity.kt
+++ app/src/main/java/us/bojie/bloom_compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import us.bojie.bloom_compose.ui.LoginPage
+import us.bojie.bloom_compose.ui.WelcomePage
 import us.bojie.bloom_compose.ui.theme.*
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BloomTheme {
-                LoginPage()
+                WelcomePage()
             }
         }
     }
