f9806f2ab2aaf55ee3e1634c279841a715a3b772
Bojie Jiang <jiangbojie@gmail.com>
2022-01-11 00:02:40 -0800

    Clear focus when search is done

--- app/src/main/java/us/bojie/bloom_compose/ui/MainPage.kt
+++ app/src/main/java/us/bojie/bloom_compose/ui/MainPage.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
@@ -36,51 +37,60 @@ fun MainPage() {
                 .padding(it)
                 .background(MaterialTheme.colors.background)
         ) {
-            var showSearchLabel by remember { mutableStateOf(true) }
-            val keyboardController = LocalSoftwareKeyboardController.current
-            val focusRequester = remember { FocusRequester() }
-
-            OutlinedTextField(
-                leadingIcon = {
-                    Row(Modifier.padding(start = 20.dp)) {
-                        Icon(
-                            painterResource(id = R.drawable.ic_search),
-                            contentDescription = "Search",
-                            Modifier.size(18.dp)
-                        )
-
-                        Spacer(modifier = Modifier.width(8.dp))
-                        AnimatedVisibility(
-                            visible = showSearchLabel,
-                            enter = fadeIn(animationSpec = tween(0)),
-                            exit = fadeOut(animationSpec = tween(0))
-                        ) {
-                            Text(
-                                text = "Search",
-                                style = MaterialTheme.typography.body1,
-                                color = MaterialTheme.colors.onPrimary,
-                            )
-                        }
-                    }
-                },
-                value = "",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .onFocusChanged { focusState ->
-                        showSearchLabel = (!focusState.isFocused)
-                    }
-                    .focusRequester(focusRequester),
-                maxLines = 1,
-                singleLine = true,
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(onDone = {
-                    keyboardController?.hide()
-                }),
-            )
+            SearchBar()
         }
     }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalComposeUiApi
+@Composable
+private fun SearchBar(modifier: Modifier = Modifier) {
+    var showSearchLabel by remember { mutableStateOf(true) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+    OutlinedTextField(
+        leadingIcon = {
+            Row(modifier.padding(start = 20.dp)) {
+                Icon(
+                    painterResource(id = R.drawable.ic_search),
+                    contentDescription = "Search",
+                    modifier.size(18.dp)
+                )
+
+                Spacer(modifier = modifier.width(8.dp))
+                AnimatedVisibility(
+                    visible = showSearchLabel,
+                    enter = fadeIn(animationSpec = tween(0)),
+                    exit = fadeOut(animationSpec = tween(0))
+                ) {
+                    Text(
+                        text = "Search",
+                        style = MaterialTheme.typography.body1,
+                        color = MaterialTheme.colors.onPrimary,
+                    )
+                }
+            }
+        },
+        value = "",
+        onValueChange = {},
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .onFocusChanged { focusState ->
+                showSearchLabel = (!focusState.isFocused)
+            }
+            .focusRequester(focusRequester),
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            focusManager.clearFocus()
+            keyboardController?.hide()
+        }),
+    )
 }
 
 @Composable
